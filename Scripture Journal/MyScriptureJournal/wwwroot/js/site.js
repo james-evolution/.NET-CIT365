74edf84d15e53759f3157c2dd6f145b0e3edce84
Jeremy <35344334+alkire-jeremy@users.noreply.github.com>
2020-02-23 06:50:18 -0600

    Added click sounds to link hovers.
    This wouldn't work well with onclick for links, because going to a new page cuts off the sound. It'd work with opening links in a new tab just fine, though. I added it to on hover instead, however.

--- Scripture Journal/MyScriptureJournal/wwwroot/js/site.js
+++ Scripture Journal/MyScriptureJournal/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var click = new Audio();
+click.src = "http://faintdev.net/academic/sounds/bg2click.mp3";
+
+function playClick() {
+    click.play();
+}
