981b141b56ba6a4fc35a2d6d9f775c9115c40204
Jeremy <35344334+alkire-jeremy@users.noreply.github.com>
2020-02-22 23:17:16 -0600

    Sorting implemented.

--- Scripture Journal/MyScriptureJournal/Pages/Books/Index.cshtml.cs
+++ Scripture Journal/MyScriptureJournal/Pages/Books/Index.cshtml.cs
@@ -22,25 +22,54 @@ namespace MyScriptureJournal.Pages.Books
         public IList<Book> Book { get;set; }
 
         [BindProperty(SupportsGet = true)]
-        public string SearchString { get; set; }
+        public string BookSearchString { get; set; }
 
         [BindProperty(SupportsGet = true)]
-        public string NoteString { get; set; }
+        public string NoteSearchString { get; set; }
+
+        public string BookSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
+
+            // If sortOrder argument is empty, default sort order to "book_descending", if it is not empty, default sort order to an empty string.
+            BookSort = String.IsNullOrEmpty(sortOrder) ? "book_descending" : "";
+
+            // If sortOrder equals date, set sortOrder to "date_descending", if it is not empty, default to "date_ascending".
+            DateSort = sortOrder == "Date" ? "date_descending" : "date_ascending";
+
             var books = from b in _context.Book
                          select b;
 
-            if (!string.IsNullOrEmpty(SearchString))
+            if (!string.IsNullOrEmpty(BookSearchString))
             {
-                books = books.Where(s => s.BookName.Contains(SearchString));
+                books = books.Where(s => s.BookName.Contains(BookSearchString));
             }
 
-            if (!string.IsNullOrEmpty(NoteString))
+            if (!string.IsNullOrEmpty(NoteSearchString))
             {
-                books = books.Where(s => s.Note.Contains(NoteString));
+                books = books.Where(s => s.Note.Contains(NoteSearchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "book_descending":
+                    books = books.OrderByDescending(b => b.BookName);
+                    break;
+                case "date_ascending":
+                    books = books.OrderBy(b => b.DateAdded);
+                    break;
+                case "date_descending":
+                    books = books.OrderByDescending(b => b.DateAdded);
+                    break;
+                default:
+                    // Sort by ascending book name, by default.
+                    books = books.OrderBy(b => b.BookName);
+                    break;
             }
 
             Book = await books.ToListAsync();
