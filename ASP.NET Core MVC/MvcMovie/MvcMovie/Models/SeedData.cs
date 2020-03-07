fe14962b56674d88b1c63de6f6906d04f25533d3
Jeremy <35344334+alkire-jeremy@users.noreply.github.com>
2020-03-07 08:27:34 -0600

    First submission. Details below.
    Meets all assignment requirements, barring:
    
    - No sorting implemented yet.
    - No color / theme changes yet.
    - No heading / content description changes yet.
    
    All on the to-do list.

---
+++ ASP.NET Core MVC/MvcMovie/MvcMovie/Models/SeedData.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "The RM",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Drama",
+                        Rating = "PG",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Other Side of Heaven",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Drama",
+                        Rating = "PG",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Meet the Mormons",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Reality",
+                        Rating = "PG",
+                        Price = 9.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ASP.NET Core MVC/MvcMovie/MvcMovie/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿function preview() {
+
+    // Use jquery to get the value of the dd element with an id of "title".
+    var movie = $("#title").text();
+
+    // Trim whitespaces before and after the string. The string comparison will not work otherwise.
+    movie = movie.trim();
+
+    if (movie === "The RM") {
+        document.getElementById("movie_preview").src = "https://m.media-amazon.com/images/M/MV5BMTI4NzQ1OTMyNV5BMl5BanBnXkFtZTYwMTE1NzQ3._V1_UX182_CR0,0,182,268_AL_.jpg";
+    }
+    else if (movie === "Other Side of Heaven") {
+        document.getElementById("movie_preview").src = "https://duckduckgo.com/i/51bc4023.png";
+    }
+    else if (movie === "Meet the Mormons") {
+        document.getElementById("movie_preview").src =
+            "https://duckduckgo.com/i/f58e1caa.jpg";
+    }
+    else {
+        document.getElementById("movie_preview").src =
+            "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcontent6.flixster.com%2Fmovie%2F11%2F03%2F44%2F11034408_det.jpg&f=1&nofb=1";
+    }
+}
+
+preview()
