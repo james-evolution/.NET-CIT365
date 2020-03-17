2d86cc71a519a9314668f5ef23e8f1b958fab78d
Jeremy <35344334+alkire-jeremy@users.noreply.github.com>
2020-03-17 07:14:20 -0500

    7/10

--- Contoso University/ContosoUniversity/ContosoUniversity/Models/Course.cs
+++ Contoso University/ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
     public class Course
     {
-        /* We'll say more about the DatabaseGenerated attribute in a later tutorial in this series.
-         * Basically, this attribute lets you enter the primary key for the course rather than having the database generate it. */
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
         public int CourseID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
+
+        [Range(0, 5)]
         public int Credits { get; set; }
 
+        public int DepartmentID { get; set; }
+
+        public Department Department { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
     }
 }
